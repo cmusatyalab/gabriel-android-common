090ef77b6540f2343e1aa569814e72cd5891e7f6
Jim Blakley <jimbla9@gmail.com>
2020-05-28 11:05:30 -0400

    Working with InfluxDB

--- client/src/main/java/edu/cmu/cs/gabriel/client/comm/TimingServerComm.java
+++ client/src/main/java/edu/cmu/cs/gabriel/client/comm/TimingServerComm.java
@@ -13,20 +13,22 @@ import android.util.LongSparseArray;
 
 import androidx.annotation.RequiresApi;
 
+
 import java.util.List;
 
+import edu.cmu.cs.cliostore.MeasurementFactory;
 import edu.cmu.cs.gabriel.client.observer.EventObserver;
 import edu.cmu.cs.gabriel.client.observer.ResultObserver;
 import edu.cmu.cs.gabriel.client.socket.TimingSocketWrapper;
 import edu.cmu.cs.gabriel.client.function.Consumer;
 import edu.cmu.cs.gabriel.protocol.Protos.ResultWrapper;
 import edu.cmu.cs.gabriel.client.comm.ScpHelper;
+import edu.cmu.cs.cliostore.InfluxDBHelper;
 
-
+@RequiresApi(api = Build.VERSION_CODES.O)
 public class TimingServerComm extends ServerCommCore {
     private final static String TAG = "TimingServerComm";
     private final static int DEFAULT_OUTPUT_FREQ = 10;
-
 
     private LongSparseArray<Long> receivedTimestamps;
     private TimingSocketWrapper timingSocketWrapper;
@@ -35,18 +37,20 @@ public class TimingServerComm extends ServerCommCore {
     private long foreverCount = 0;
     private long startTime;
     private long intervalStartTime;
-    private static ScpHelper scphelper = new ScpHelper();
-    private static Log4jHelper loghelper = new Log4jHelper();
-    private static LocationHelper locHelper = new LocationHelper();
 
-    private static final long createTime = System.currentTimeMillis();
-    private static final long EPOCHLENGTH = 240; // Every N * 10 Frames -- 240 is 2 minutes at 20 fps
     private static final String LOGTAG = "TimingServerComm";
-    private static String MSGTAG = "";
-    private static final String dl = "|";
+    private static Log4jHelper loghelper = new Log4jHelper();
+    private static org.apache.log4j.Logger log = loghelper.getLogger( LOGTAG );
     private static String logfname = loghelper.getFname();
 
-    private static org.apache.log4j.Logger log = loghelper.getLogger( LOGTAG );
+    private static LocationHelper locHelper = new LocationHelper();
+    private static final long createTime = System.currentTimeMillis();
+    private static final long EPOCHLENGTH = 240; // Every N * 10 Frames -- 240 is 2 minutes at 20 fps
+
+    private static String MSGTAG = "";
+    private static final String dl = "|";
+    private static ScpHelper scphelper = new ScpHelper();
+    private static InfluxDBHelper influxhelper = new InfluxDBHelper();
 
     // TODO: Replace these constructors with a builder to allow setting tokenLimit without setting
     //       outputFreq
@@ -57,16 +61,16 @@ public class TimingServerComm extends ServerCommCore {
         super(tokenLimit);
 
         this.receivedTimestamps = new LongSparseArray<>();
-
+//        this.influxhelper.listDB();
+        this.influxhelper.dropMeasurementRecords("location");
+        this.influxhelper.dropMeasurementRecords("framerate");
+        this.influxhelper.dropMeasurementRecords("roundtriptime"); // Clear it out
         NetworkInfo active_network = conMan.getActiveNetworkInfo();
         String connect_state = active_network.getState().name();
         String connect_type = active_network.getTypeName();
 
         // Create tag showing device, server and network data
-        MSGTAG = dl + Build.MANUFACTURER + dl;
-        MSGTAG += Build.MODEL + dl;
-        MSGTAG += serverURL + dl;
-        MSGTAG += connect_type + dl;
+        MSGTAG = String.format("|%s|%s|%s|%s|",Build.MANUFACTURER,Build.MODEL,serverURL,connect_type);
 
         locMan.requestLocationUpdates("gps",1000, (float) 1, locHelper);
         List<String> locProviders = locMan.getAllProviders();
@@ -91,6 +95,8 @@ public class TimingServerComm extends ServerCommCore {
                     Location locationGPS = locMan.getLastKnownLocation(LocationManager.GPS_PROVIDER);
                     if (locationGPS != null) {
                         msg = String.format("Current GPS LONG/LAT: POINT(%f %f)",locationGPS.getLongitude(),locationGPS.getLatitude());
+                        influxhelper.addPoint("location",Build.MANUFACTURER,Build.MODEL,serverURL, connect_type, locationGPS.getLongitude(),locationGPS.getLatitude());
+//                        Point locpoint = mfact.makeLocationMeasurement(locationGPS.getLongitude(),locationGPS.getLatitude());
                     } else {
                         msg = "Current GPS LAT/LNG: UNAVAILABLE";
                     }
@@ -109,11 +115,13 @@ public class TimingServerComm extends ServerCommCore {
                             ((timestamp - intervalStartTime) / 1000.0));
 //                    Log.i(TAG, "Interval FPS: " + intervalFps);
                     log.info(TimingServerComm.this.MSGTAG + "IntervalFPS" + dl  + intervalFps);
+                    influxhelper.addPoint("framerate",Build.MANUFACTURER,Build.MODEL,serverURL, connect_type, overallFps,intervalFps);
 
                     // Log Round Trip Time (RTT) Data
                     double[] artt = TimingServerComm.this.logAvgRtt();
                     log.info(TimingServerComm.this.MSGTAG + "OverallRTT(ms)" + dl + artt[0]);
                     log.info(TimingServerComm.this.MSGTAG + "IntervalRTT(ms)" + dl + artt[1]);
+                    influxhelper.addPoint("roundtriptime",Build.MANUFACTURER,Build.MODEL,serverURL, connect_type, artt[0],artt[1]);
 
                     TimingServerComm.this.intervalCount = 0;
                     // TODO: change to java.time.Instant once we can stop supporting Google Glass
